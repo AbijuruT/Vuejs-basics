934231a332ebf25c96a4395ac4310e3f016c903f
AbijuruT <abijuruyves@gmail.com>
2023-09-24 19:18:58 +0200

    Adding vue js

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-const buttonEl = document.querySelector('button')
-const inputEl = document.querySelector('input')
-const listEl = document.querySelector('ul')
+/**
+ * Using vue js
+ */
+Vue.createApp({
+    data(){
+        return {
+            goals: [],
+            enteredValue: ''
+        }
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue)
+            this.enteredValue = ''
+        }
+    }
+}).mount('#app')
 
-function addGoal(){
-    const enteredValue = inputEl.value
-    const listItemEl = document.createElement('li')
-    listItemEl.textContent = enteredValue
-    listEl.appendChild(listItemEl)
-    inputEl.value = ''
-}
 
-buttonEl.addEventListener('click', addGoal)
+/**
+ * The regular javascript code
+ */
+// const buttonEl = document.querySelector('button')
+// const inputEl = document.querySelector('input')
+// const listEl = document.querySelector('ul')
+
+// function addGoal(){
+//     const enteredValue = inputEl.value
+//     const listItemEl = document.createElement('li')
+//     listItemEl.textContent = enteredValue
+//     listEl.appendChild(listItemEl)
+//     inputEl.value = ''
+// }
+
+// buttonEl.addEventListener('click', addGoal)
